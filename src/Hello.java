a145a4828d5ea2e5af14358bf78481c13754f735
TempAd <TempAd@Flex.unl.edu>
2026-08-31 20:59:08 -0500

    added major

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Aygozel Berdiyeva (aberdiyeva2@huskers.unl.edu)
+ * Date: 2026/08/30
+ *
+ * name printing program
+ *
+ *
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Aygozel Berdiyeva");
+	}
+}
